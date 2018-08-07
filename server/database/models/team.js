5914ccda9218a2574099fb21598a815481968823
Ouassim <ouassim.ben-mosbah@etu.umontpellier.fr>
2018-08-07 13:58:07 +0200

    feat(TeamModel): add team model

--- server/database/models/team.js
+++ server/database/models/team.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const User = require('./user');
+const User = require('mongoose').model('User').schema;
 mongoose.promise = Promise;
 
 // Define teamSchema
