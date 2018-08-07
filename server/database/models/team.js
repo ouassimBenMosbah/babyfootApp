4c6b389869a67c5ea89f4da2160d345ed0162843
Ouassim <ouassim.ben-mosbah@etu.umontpellier.fr>
2018-08-07 13:23:53 +0200

    feat(Model): populate models

---
+++ server/database/models/team.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const User = require('./user');
+mongoose.promise = Promise;
+
+// Define teamSchema
+const teamSchema = new Schema({
+	users: { type: [User], unique: true, required: true }
+});
+
+// Define schema methods
+teamSchema.methods = {};
+
+// Define hooks for pre-saving
+teamSchema.pre('save', function (next) {
+	if (!this.users || this.users.length === 0) {
+		console.log('models/team.js =======NO USERS PROVIDED=======');
+		next();
+	} else {
+		next();
+	}
+})
+
+const Team = mongoose.model('Team', teamSchema);
+module.exports = Team;
