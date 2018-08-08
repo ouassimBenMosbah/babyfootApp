55a534a4793771ac2db56c984a4571daa42280b4
lfroment <loic.p.a.froment@gmail.com>
2018-08-08 13:23:56 +0200

    refactor: export app from app.js and import app in server.js
    server.js is now the entry point of the application
    
    This commit also updates the start script in package.json

---
+++ server/app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import session from 'express-session';
+import connectMongo from 'connect-mongo';
+import dbConnection from './database';
+import passport from './passport';
+import user from './routes/user';
+
+const app = express();
+const MongoStore = connectMongo(session);
+
+
+app.use(morgan('dev'));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+);
+app.use(bodyParser.json());
+
+app.use(
+  session({
+    secret: 'fraggle-rock',
+    store: new MongoStore({ mongooseConnection: dbConnection }),
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/user', user);
+
+export default app;
--- server/server.js
+++ server/server.js
@@ -1,36 +1,7 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import morgan from 'morgan';
-import session from 'express-session';
-import connectMongo from 'connect-mongo';
-import dbConnection from './database';
-import passport from './passport';
-import user from './routes/user';
+import app from './app';
 
-const app = express();
-const MongoStore = connectMongo(session);
+const PORT = 8080;
 
-
-app.use(morgan('dev'));
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  }),
-);
-app.use(bodyParser.json());
-
-app.use(
-  session({
-    secret: 'fraggle-rock',
-    store: new MongoStore({ mongooseConnection: dbConnection }),
-    resave: false,
-    saveUninitialized: false,
-  }),
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use('/user', user);
-
-app.listen(8080);
+app.listen(PORT, () => {
+  console.log('Server is listening on port', PORT);
+});
