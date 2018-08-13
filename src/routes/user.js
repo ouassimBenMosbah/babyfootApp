5aca24d1af16a3a1131191f8abe22542181ff3f2
lfroment <loic.p.a.froment@gmail.com>
2018-08-13 15:26:35 +0200

    feat(user): enable fielded query and pagination for get users route

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,38 +4,37 @@ import User from '../database/models/user';
 const router = express.Router();
 const passport = require('../passport');
 
-router.post(
-  '/login',
-  (req, res, next) => {
-    console.log('routes/user.js, login, req.body: ');
-    console.log(req.body);
-    next();
-  },
-  passport.authenticate('local'),
-  (req, res) => {
-    console.log('logged in', req.body.username);
-    const userInfo = {
-      username: req.user.username,
-    };
-    res.send(userInfo);
-  },
-);
-
-router.get('/', (req, res) => {
-  User.find({}).then((users) => {
-    res.status(200).send(users);
-  }).catch((err) => {
-    res.status(401).send('An error occured', err);
-  });
-});
-
 const arrayToObject = (array) => {
-  const fields = array.forEach((field) => {
+  const fields = {};
+  array.forEach((field) => {
     fields[field] = 1;
     return fields;
   }, {});
   return fields;
 };
+
+router.get('/', async (req, res) => {
+  const { range } = req.query;
+  const [rangeStart, rangeEnd] = range ? range.split('-') : [null, null];
+  const { fields } = req.query;
+
+  let dict = {};
+
+  if (fields) {
+    dict = arrayToObject(fields.split(','));
+    console.log(dict);
+  }
+  const users = await User.find()
+    .skip(+rangeStart)
+    .limit(+rangeEnd)
+    .select(dict);
+  if (users) {
+    res.send(users);
+  } else {
+    res.status(404).send('User not found');
+  }
+  // return res.status(200).json(users);
+});
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
